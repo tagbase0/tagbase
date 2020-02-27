8a0480bfd95711c354bea307a5a5eb92c40e6b53
zerotom <690962991@qq.com>
2020-02-27 11:26:26 +0800

    storage module modify

--- storage-hbase/src/main/java/com/oppo/tagbase/storage/hbase/HbaseStorageConnector.java
+++ storage-hbase/src/main/java/com/oppo/tagbase/storage/hbase/HbaseStorageConnector.java
@@ -1,14 +1,14 @@
 package com.oppo.tagbase.storage.hbase;
 
-import com.google.common.collect.Range;
 import com.oppo.tagbase.meta.obj.ColumnType;
 import com.oppo.tagbase.storage.core.connector.StorageConnector;
+import com.oppo.tagbase.storage.core.exception.StorageErrorCode;
 import com.oppo.tagbase.storage.core.exception.StorageException;
-import com.oppo.tagbase.storage.core.obj.DimContext;
 import com.oppo.tagbase.storage.core.obj.OperatorBuffer;
-import com.oppo.tagbase.storage.core.obj.RawRow;
-import com.oppo.tagbase.storage.core.obj.StorageQueryContext;
 import com.oppo.tagbase.storage.core.util.BitmapUtil;
+import com.oppo.tagbase.storage.core.obj.RawRow;
+import com.oppo.tagbase.storage.core.obj.DimContext;
+import com.oppo.tagbase.storage.core.obj.StorageQueryContext;
 import com.oppo.tagbase.storage.core.util.StorageConstant;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -23,6 +23,7 @@ import org.roaringbitmap.buffer.ImmutableRoaringBitmap;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by liangjingya on 2020/2/8.
@@ -36,23 +37,28 @@ public class HbaseStorageConnector extends StorageConnector {
 
     private Admin admin = null;
 
-    public static String REGEX_START_STR = "^";
-    public static String REGEX_ANY_STR = ".+";
-    public static String REGEX_END_STR = "$";
-
+    /**
+     * init hbase connector
+     */
     @Override
-    protected void initConnector() {
+    protected void initConnector() throws StorageException  {
         log.info("init hbase Connector!");
         log.info(hbaseConfig.toString());
         initHbaseConnection();
     }
 
+    /**
+     * destroy hbase connector
+     */
     @Override
     protected void destroyConnector() {
         log.info("destroy hbase Connector!");
         closeHbaseConnection();
     }
 
+    /**
+     * create hbase table
+     */
     @Override
     protected void createTable(String dbName, String tableName, int partition) throws StorageException {
         List<String> familys = new ArrayList<>();
@@ -60,93 +66,86 @@ public class HbaseStorageConnector extends StorageConnector {
         createHbaseTableIfNotExist(dbName, tableName, familys, getSplitKeys(partition));
     }
 
+    /**
+     * delete hbase table
+     */
     @Override
     public void deleteTable(String dbName, String tableName) throws StorageException {
         deleteHbaseTable(hbaseConfig.getNameSpace(), tableName);
     }
 
+    /**
+     * hbase put data
+     */
     @Override
     public void createRecord(String dbName, String tableName, String key, ImmutableRoaringBitmap value) throws StorageException {
         try {
             byte[] metric = BitmapUtil.serializeBitmap(value);
             put(hbaseConfig.getNameSpace(), tableName, key, hbaseConfig.getFamily(), hbaseConfig.getQualifier(), metric);
         } catch (Exception e) {
-            throw new StorageException("hbaseStorageConnector createRecord error",e);
+            throw new StorageException(StorageErrorCode.STORAGE_INSERT_ERROR, e, "hbaseStorageConnector createRecord error");
         }
 
     }
 
+    /**
+     * hbase bulkload data
+     */
     @Override
     public void createBatchRecords(String dbName, String tableName, String dataPath) throws StorageException {
         createTable(hbaseConfig.getNameSpace(), tableName);
         bulkLoad(tableName, dataPath);
     }
 
+    /**
+     * hbase query data
+     */
     @Override
     protected void createStorageQuery(StorageQueryContext storageQueryContext, OperatorBuffer<RawRow> buffer) throws StorageException {
 
         String tableName = storageQueryContext.getSliceSegment().getTableName();
         String dayNumValue = storageQueryContext.getSliceSegment().getSliceDate();
-        //构造scan filter
-        FilterList rowFilterList = new FilterList();
-        //dim索引关系(index,returnIndex)
-        Map<Integer,Integer> indexMap = new HashMap<>();
-        String scanRegexStr = createScanRegexStr(storageQueryContext, indexMap);
-        log.debug("scan filter regex string : " + scanRegexStr);
-        Filter regexFilter = new RowFilter(CompareFilter.CompareOp.EQUAL,new RegexStringComparator(scanRegexStr));
-        rowFilterList.addFilter(regexFilter);
-
-        scan(hbaseConfig.getNameSpace(), tableName, hbaseConfig.getFamily(), hbaseConfig.getQualifier(), hbaseConfig.getRowkeyDelimiter(), rowFilterList, indexMap, dayNumValue, buffer);
-    }
-
-
-    private String createScanRegexStr(StorageQueryContext storageQueryContext, Map<Integer,Integer> indexMap){
-
-        int segmentId = storageQueryContext.getSliceSegment().getSegmentId();
         int index = 1;
-        StringBuilder builder = new StringBuilder();
-        builder.append(REGEX_START_STR + segmentId + hbaseConfig.getRowkeyDelimiter());
-        for(DimContext meta : storageQueryContext.getDimContextList()){
-            if(meta.getType() == ColumnType.DIM_COLUMN ) {
-                if(meta.getDimReturnIndex() != StorageConstant.FLAG_NO_NEED_RETURN){
-                    indexMap.put(index, meta.getDimReturnIndex());
+        Map<Integer,Integer> indexMap = new HashMap<>();
+        boolean equalComparatorFlag = true;
+        //遍历出所有的rowkey组合, 封装成list
+        List<String> rowkeyPrefixList = new ArrayList(){{add(String.valueOf(storageQueryContext.getSliceSegment().getSegmentId()));}};
+        for(DimContext column : storageQueryContext.getDimContextList()){
+            if(column.getType() == ColumnType.DIM_COLUMN){
+                if(column.getDimReturnIndex() != StorageConstant.FLAG_NO_NEED_RETURN){
+                    indexMap.put(index, column.getDimReturnIndex());
+                    index++;
                 }
-                if (meta.getDimValues() == null) {
-                    if (!builder.toString().endsWith(REGEX_ANY_STR)) {
-                        builder.append(REGEX_ANY_STR);
-                    }
-                } else {
-                    int size = meta.getDimValues().asRanges().size();
-                    if (size > 0) {
-                        builder.append("(");
-                        int k = 1;
-                        for (Range<String> value : meta.getDimValues().asRanges()) {
-                            builder.append(value.lowerEndpoint());
-                            if(k != size){
-                                builder.append("|");
-                            }
-                            k++;
-                        }
-                        builder.append(")");
-                        builder.append(hbaseConfig.getRowkeyDelimiter());
-                    }
+                if(column.getDimValues() != null) {
+                    List<String> finalRowkeyPrefixList = rowkeyPrefixList;
+                    List<String> newRowkeyPrefixList = column.getDimValues().asRanges().stream()
+                            .flatMap(value -> {
+                                List<String> tmpRowkeyPrefixList = new ArrayList<>();
+                                finalRowkeyPrefixList.stream()
+                                        .forEach(oldRowkey -> {
+                                            tmpRowkeyPrefixList.add(oldRowkey + hbaseConfig.getRowkeyDelimiter() + value.lowerEndpoint());
+                                        });
+                                return tmpRowkeyPrefixList.stream();
+                            }).collect(Collectors.toList());
+                    rowkeyPrefixList.clear();
+                    rowkeyPrefixList = newRowkeyPrefixList;
+                }else {
+                    equalComparatorFlag = false;
                 }
-                index++;
-            }else if(meta.getType() == ColumnType.SLICE_COLUMN ){
-                indexMap.put(meta.getDimIndex(), meta.getDimReturnIndex());
-            }
-            if(index >= storageQueryContext.getDimContextList().size() &&
-                    builder.toString().endsWith(hbaseConfig.getRowkeyDelimiter())){
-                builder.deleteCharAt(builder.length() - 1);
+            }else if(column.getType() == ColumnType.SLICE_COLUMN){
+                if(column.getDimReturnIndex() != StorageConstant.FLAG_NO_NEED_RETURN){
+                    indexMap.put(column.getDimIndex(), column.getDimReturnIndex());
+                }
             }
         }
-        if(!builder.toString().endsWith(REGEX_ANY_STR)){
-            builder.append(REGEX_END_STR);
-        }
-        return builder.toString();
+
+        log.debug("rowkey prefix list {}", rowkeyPrefixList);
+        scan(hbaseConfig.getNameSpace(), tableName, hbaseConfig.getFamily(), hbaseConfig.getQualifier(), hbaseConfig.getRowkeyDelimiter(), equalComparatorFlag, rowkeyPrefixList, indexMap, dayNumValue, buffer);
+
     }
 
-    private void initHbaseConnection() {
+
+    private void initHbaseConnection() throws StorageException {
         try {
             Configuration conf = HBaseConfiguration.create();
             conf.set("hbase.zookeeper.property.clientPort", hbaseConfig.getZkPort());
@@ -155,7 +154,7 @@ public class HbaseStorageConnector extends StorageConnector {
             connection = ConnectionFactory.createConnection(conf);
             admin = connection.getAdmin();
         } catch (IOException e) {
-            log.error("init hbase connection error", e);
+            throw new StorageException(StorageErrorCode.INIT_STORAGE_ERROR, e, "init hbase connection error");
         }
     }
 
@@ -173,13 +172,16 @@ public class HbaseStorageConnector extends StorageConnector {
 
     }
 
+    /**
+     * create region split key
+     */
     private byte[][] getSplitKeys(int partition){
         if(partition <= 1){
             return null;
         }
         byte[][] splitKeys = new byte[partition-1][];
         for(int i=1; i<=partition; i++){
-            splitKeys[i] = Bytes.toBytes((i+1)+"");
+            splitKeys[i] = Bytes.toBytes((i + 1) + "");
         }
         return splitKeys;
     }
@@ -199,8 +201,8 @@ public class HbaseStorageConnector extends StorageConnector {
                 }
                 admin.createTable(tableDesc, splitKeys);
             }
-        }catch (Exception e){
-            throw new StorageException("hbaseStorageConnector createTable error",e);
+        }catch (IOException e){
+            throw new StorageException(StorageErrorCode.STORAGE_TABLE_ERROR, e, "hbaseStorageConnector createTable error");
         }
     }
 
@@ -213,8 +215,8 @@ public class HbaseStorageConnector extends StorageConnector {
                 }
             }
             admin.createNamespace(NamespaceDescriptor.create(nameSpace).build());
-        } catch (Exception e) {
-            throw new StorageException("hbaseStorageConnector createNamespace error",e);
+        } catch (IOException e) {
+            throw new StorageException(StorageErrorCode.STORAGE_TABLE_ERROR, e, "hbaseStorageConnector createNamespace error",e);
         }
 
     }
@@ -228,8 +230,8 @@ public class HbaseStorageConnector extends StorageConnector {
                 admin.disableTable(tName);
                 admin.deleteTable(tName);
             }
-        } catch (Exception e) {
-            throw new StorageException("hbaseStorageConnector deleteHbaseTable error",e);
+        } catch (IOException e) {
+            throw new StorageException(StorageErrorCode.STORAGE_TABLE_ERROR, e, "hbaseStorageConnector deleteHbaseTable error",e);
         }
     }
 
@@ -242,7 +244,7 @@ public class HbaseStorageConnector extends StorageConnector {
             put.addColumn(Bytes.toBytes(family), Bytes.toBytes(qualifier), value);
             table.put(put);
         }catch (IOException e){
-            throw new StorageException("hbaseStorageConnector put error",e);
+            throw new StorageException(StorageErrorCode.STORAGE_INSERT_ERROR, e, "hbaseStorageConnector put error");
         }finally {
             if(table != null) {
                 try {
@@ -268,7 +270,7 @@ public class HbaseStorageConnector extends StorageConnector {
                 return Bytes.toString(CellUtil.cloneValue(cell));
             }
         }catch (Exception e){
-            throw new StorageException("hbaseStorageConnector get error",e);
+            throw new StorageException(StorageErrorCode.STORAGE_QUERY_ERROR, e, "hbaseStorageConnector get error");
         }finally {
             if(table != null) {
                 try {
@@ -288,49 +290,59 @@ public class HbaseStorageConnector extends StorageConnector {
             TableName table = TableName.valueOf(hbaseConfig.getNameSpace(), tableName);
             loader.doBulkLoad(new Path(dataPath), admin, connection.getTable(table), connection.getRegionLocator(table));
         }catch (Exception e){
-            throw new StorageException("hbaseStorageConnector bulkLoad error",e);
+            throw new StorageException(StorageErrorCode.STORAGE_INSERT_ERROR, e, "hbaseStorageConnector bulkLoad error");
         }
     }
 
 
-    public void scan(String nameSpace, String tableName, String family, String qualifier, String delimiter, FilterList filterList, Map<Integer,Integer> indexMap, String dayNumValue, OperatorBuffer<RawRow> buffer) throws StorageException {
+    public void scan(String nameSpace, String tableName, String family, String qualifier, String delimiter, boolean equalComparatorFlag, List<String> rowkeyPrefixList, Map<Integer,Integer> indexMap, String dayNumValue, OperatorBuffer<RawRow> buffer) throws StorageException {
 
         ResultScanner scanner = null;
         try {
             Table table = connection.getTable(TableName.valueOf(nameSpace, tableName));
             Scan scan = new Scan();
+            //设置缓冲
             scan.setCaching(hbaseConfig.getScanCacheSize());
             scan.setMaxResultSize(hbaseConfig.getScanMaxResultSize());
-            scan.setFilter(filterList);
-            scan.addColumn(family.getBytes(), qualifier.getBytes());
-            scanner = table.getScanner(scan);
-            if (scanner != null) {
-                Iterator<Result> iterator = scanner.iterator();
-                while (iterator.hasNext()) {
-                    for (Cell cell : iterator.next().rawCells()) {
-                        //切分rowkey
-                        String[] dimValueArr = Bytes.toString(cell.getRowArray(), cell.getRowOffset(), cell.getRowLength()).split(delimiter);
 
-                        byte[][] dimValues = null;
-                        if(indexMap.size() > 0 ) {
-                            dimValues = new byte[indexMap.size()][];
-                            for (int index : indexMap.keySet()) {
-                                if(index == StorageConstant.SLICE_COLUMN_INDEX){//如果需要返回sliceColumn
-                                    dimValues[indexMap.get(index)-1] = dayNumValue.getBytes();
-                                }else {
-                                    dimValues[indexMap.get(index)-1] = dimValueArr[index].getBytes();
+            for(String rowkeyPrefix : rowkeyPrefixList) {
+                //设置startkey和endkey
+                scan.setRowPrefixFilter(rowkeyPrefix.getBytes());
+                //如果是等值查找，再设置一个BinaryComparator
+                if (equalComparatorFlag) {
+                    Filter rowFilter = new RowFilter(CompareFilter.CompareOp.EQUAL, new BinaryComparator(rowkeyPrefix.getBytes()));
+                    scan.setFilter(rowFilter);
+                }
+                scan.addColumn(family.getBytes(), qualifier.getBytes());
+                scanner = table.getScanner(scan);
+                if (scanner != null) {
+                    Iterator<Result> iterator = scanner.iterator();
+                    while (iterator.hasNext()) {
+                        for (Cell cell : iterator.next().rawCells()) {
+                            //切分rowkey
+                            String[] dimValueArr = Bytes.toString(cell.getRowArray(), cell.getRowOffset(), cell.getRowLength()).split(delimiter);
+
+                            byte[][] dimValues = null;
+                            if (indexMap.size() > 0) {
+                                dimValues = new byte[indexMap.size()][];
+                                for (int index : indexMap.keySet()) {
+                                    if (index == StorageConstant.SLICE_COLUMN_INDEX) {//如果需要返回sliceColumn
+                                        dimValues[indexMap.get(index) - 1] = dayNumValue.getBytes();
+                                    } else {
+                                        dimValues[indexMap.get(index) - 1] = dimValueArr[index].getBytes();
+                                    }
                                 }
                             }
+                            ImmutableRoaringBitmap bitmap = BitmapUtil.deSerializeBitmap(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength());
+                            RawRow rawRow = new RawRow(dimValues, bitmap);
+                            buffer.postData(rawRow);
                         }
-                        ImmutableRoaringBitmap bitmap = BitmapUtil.deSerializeBitmap(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength());
-                        RawRow RawRow = new RawRow(dimValues, bitmap);
-                        buffer.postData(RawRow);
                     }
                 }
             }
             buffer.postEnd();
         }catch (Exception e){
-            StorageException exception = new StorageException("hbaseStorageConnector scan error",e);
+            StorageException exception = new StorageException(StorageErrorCode.STORAGE_QUERY_ERROR, e, "hbaseStorageConnector scan error");
             buffer.fastFail(exception);
             throw exception;
         }finally {
